bd6ba6c307773bd850f83adde669d0d4729c9e50
Anakharsis9 <dayana530807@gmail.com>
2025-03-09 15:59:08 +0500

    feat: update files structure

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import fs from 'fs';
+import parse from './parser.js';
+import format from './formatters/index.js';
+import buildDiff from './buildDiff.js';
+
+const getFormat = (filepath) => path.extname(filepath).toLowerCase().slice(1);
+
+const readFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  return fs.readFileSync(absolutePath, 'utf8');
+};
+
+const parseFile = (filepath) => {
+  const fileContent = readFile(filepath);
+  const fileFormat = getFormat(filepath);
+  return parse(fileContent, fileFormat);
+};
+
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const data1 = parseFile(filepath1);
+  const data2 = parseFile(filepath2);
+
+  const diff = buildDiff(data1, data2);
+  return format(diff, formatName);
+};
+
+export default genDiff;
