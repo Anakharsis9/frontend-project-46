b0e318042ef35ce0a242d8cacf7577ddfdf9e7ed
Anakharsis9 <dayana530807@gmail.com>
2025-03-10 16:41:14 +0500

    refactor: plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,38 +1,39 @@
+/* eslint-disable no-use-before-define */
 import _ from 'lodash';
 
+const formatValue = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  return typeof value === 'string' ? `'${value}'` : String(value);
+};
+
+const formatDiffRecord = (record, parentPath) => {
+  const propertyPath = parentPath ? `${parentPath}.${record.key}` : record.key;
+
+  switch (record.type) {
+    case 'added':
+      return `Property '${propertyPath}' was added with value: ${formatValue(
+        record.value,
+      )}`;
+    case 'removed':
+      return `Property '${propertyPath}' was removed`;
+    case 'changed':
+      return `Property '${propertyPath}' was updated. From ${formatValue(
+        record.value1,
+      )} to ${formatValue(record.value2)}`;
+    case 'nested':
+      return formatPlain(record.children, propertyPath);
+    case 'unchanged':
+      return null;
+    default:
+      throw new Error(`Unknown type: ${record.type}`);
+  }
+};
+
 const formatPlain = (diff, parentPath = '') => {
-  const formatValue = (value) => {
-    if (_.isObject(value)) {
-      return '[complex value]';
-    }
-    return typeof value === 'string' ? `'${value}'` : value;
-  };
-
-  return diff
-    .map((record) => {
-      const fullPath = parentPath ? `${parentPath}.${record.key}` : record.key;
-
-      switch (record.type) {
-        case 'added':
-          return `Property '${fullPath}' was added with value: ${formatValue(
-            record.value,
-          )}`;
-        case 'removed':
-          return `Property '${fullPath}' was removed`;
-        case 'changed':
-          return `Property '${fullPath}' was updated. From ${formatValue(
-            record.value1,
-          )} to ${formatValue(record.value2)}`;
-        case 'nested':
-          return formatPlain(record.children, fullPath);
-        case 'unchanged':
-          return null;
-        default:
-          throw new Error(`Unknown type: ${record.type}`);
-      }
-    })
-    .filter(Boolean)
-    .join('\n');
+  const result = diff.map((record) => formatDiffRecord(record, parentPath));
+  return result.filter(Boolean).join('\n');
 };
 
 export default formatPlain;
