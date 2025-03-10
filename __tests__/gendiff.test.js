b06cf873c804193a746c04fe8403b9183116ffeb
Anakharsis9 <dayana530807@gmail.com>
2025-03-10 14:17:42 +0500

    refactor: remove expected

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,7 +6,7 @@ import genDiff from '../index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const readFixture = (filename) => fs.readFileSync(path.join(__dirname, '__expected__', filename), 'utf-8');
+const readFixture = (filename) => fs.readFileSync(path.join(__dirname, '__fixtures__', filename), 'utf-8');
 
 const expectedPlain = readFixture('expected_plain.txt');
 const expectedStylish = readFixture('expected_stylish.txt');
