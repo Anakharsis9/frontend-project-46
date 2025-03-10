ab51439a2bc46a44c630c379ab3e8a17f0200a90
Anakharsis9 <dayana530807@gmail.com>
2025-03-10 16:30:45 +0500

    refactor: stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,59 +1,55 @@
+/* eslint-disable no-use-before-define */
 import _ from 'lodash';
 
+const calcMargin = (level, spaceCountByLevel = 4, leftShift = 2) => ' '.repeat(level * spaceCountByLevel - leftShift);
+
+const stringify = (value, level) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+
+  const entries = Object.entries(value)
+    .map(
+      ([key, val]) => `${calcMargin(level + 1)}  ${key}: ${stringify(val, level + 1)}`,
+    )
+    .join('\n');
+
+  return `{
+${entries}
+${calcMargin(level)}  }`;
+};
+
+const formatDiffRecord = (record, level) => {
+  const margin = calcMargin(level);
+
+  switch (record.type) {
+    case 'removed':
+      return `${margin}- ${record.key}: ${stringify(record.value, level)}`;
+    case 'added':
+      return `${margin}+ ${record.key}: ${stringify(record.value, level)}`;
+    case 'nested':
+      return `${margin}  ${record.key}: {
+${formatStylish(record.children, level + 1)}
+${margin}  }`;
+    case 'changed':
+      return `${margin}- ${record.key}: ${stringify(
+        record.value1,
+        level,
+      )}\n${margin}+ ${record.key}: ${stringify(record.value2, level)}`;
+    case 'unchanged':
+      return `${margin}  ${record.key}: ${stringify(record.value, level)}`;
+    default:
+      throw new Error(`Unknown type: ${record.type}`);
+  }
+};
+
 const formatStylish = (diff, deepLevel = 1) => {
-  const spaceCountByLevel = 4;
-  const leftShift = 2;
-  const calcMargin = (level) => ' '.repeat(level * spaceCountByLevel - leftShift);
-
-  const stringify = (value, level) => {
-    if (!_.isObject(value)) {
-      return value;
-    }
-
-    const entries = Object.entries(value)
-      .map(
-        ([key, val]) => `${calcMargin(level + 1)}  ${key}: ${stringify(val, level + 1)}`,
-      )
-      .join('\n');
-
-    return `{\n${entries}\n${calcMargin(level)}  }`;
-  };
-
-  const result = diff.map((record) => {
-    const margin = calcMargin(deepLevel);
-
-    switch (record.type) {
-      case 'removed':
-        return `${margin}- ${record.key}: ${stringify(
-          record.value,
-          deepLevel,
-        )}`;
-      case 'added':
-        return `${margin}+ ${record.key}: ${stringify(
-          record.value,
-          deepLevel,
-        )}`;
-      case 'nested':
-        return `${margin}  ${record.key}: {\n${formatStylish(
-          record.children,
-          deepLevel + 1,
-        )}\n${margin}  }`;
-      case 'changed':
-        return `${margin}- ${record.key}: ${stringify(
-          record.value1,
-          deepLevel,
-        )}\n${margin}+ ${record.key}: ${stringify(record.value2, deepLevel)}`;
-      case 'unchanged':
-        return `${margin}  ${record.key}: ${stringify(
-          record.value,
-          deepLevel,
-        )}`;
-      default:
-        throw new Error(`Unknown type: ${record.type}`);
-    }
-  });
-
-  return deepLevel === 1 ? `{\n${result.join('\n')}\n}` : result.join('\n');
+  const result = diff.map((record) => formatDiffRecord(record, deepLevel));
+  return deepLevel === 1
+    ? `{
+${result.join('\n')}
+}`
+    : result.join('\n');
 };
 
 export default formatStylish;
